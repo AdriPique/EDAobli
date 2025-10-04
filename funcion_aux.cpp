8e44a07bef9d74b845d8d7f3281f02ac04e9c1e8
Juan Cabot <juancabot09@gmail.com>
2025-10-03 23:32:24 -0300

    Posibles estructuras e inicio de implementacion de funciones

---
+++ funcion_aux.cpp
@@ -0,0 +1,17 @@
+#include "archivo.h"
+#include "version.h"
+#include <iostream>
+using namespace std;
+
+
+void imprimir_versiones(version v){
+    cout << endl;
+    for(int i=v->nivel; i>0; i--){
+        cout << "   ";
+    }
+    cout << v-> 
+    if(v->)
+}
+
+
+// bool existe_version(archivo, char* version)
---
+++ texto.cpp
@@ -0,0 +1,9 @@
+#include "texto.h"
+
+
+//Representa el texto como una lista, nodo=linea.
+struct nodo_texto{
+    int num_linea;
+    char* linea;
+    texto sig_linea;
+};
