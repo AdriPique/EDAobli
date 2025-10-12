df52e35ee2ed0bcbc9da1ff16150ec3903683e9b
AdriPique <adripique04@gmail.com>
2025-10-12 09:00:35 -0300

    Cree una version "guerrera " de Existe version (falta probar)

--- IdeasAdri.cpp
+++ IdeasAdri.cpp
@@ -19,6 +19,7 @@ struct Versiones{
     char* nombre;
     Version padre, hijo, hermano;
     Linea linea;
+    int numero;
 };
 
 typedef enum _retorno TipoRet;
@@ -27,23 +28,86 @@ enum _retorno{
 OK, ERROR, NO_IMPLEMENTADA
 };
 
-int recorrerVersiones(char * version,Archivo a){
+bool tieneHijo(Versiones v){
+    return(v.hijo!=NULL);
+}
+
+
+
+char versionNumerica(Versiones v ){
+    char * nodo=new char; 
+
+}
+
+/*Archivo recorrerVersiones(char * version,Archivo a){
     char* parteVersion = strtok(version,".");
     int num1=atoi(parteVersion);
-    char* versionact = strtok(a.versiones->nombre,".");//por que me deja usar punto y  no flecha?????
-    int num2=atoi(versionact);
-    
-    if(num1==num2){
-        while(parteVersion!=NULL and versionact!=NULL){
-            parteVersion = strtok(version,NULL);
+    int num2=a.versiones->numero;// LA CONCHA DEL PUNTO
+    while(parteVersion!=NULL){
+        if(num1==num2){
+            parteVersion=strtok(NULL,".");
             num1=atoi(parteVersion);
-            versionact = strtok(a.versiones->nombre,NULL);//por que me deja usar punto y  no flecha?????
-            num2=atoi(versionact);
-            
+            if (num1 > a.versiones->hijo->numero){
+                num2=a.versiones->hermano->numero;
+            }else if(num1 > a.versiones->hermano->numero){
+                num2=a.versiones->hijo->numero;
+            }
+        }
+    }
+    
+}*/
+/*int tokenVersion( char * version){
+    //te devuelve de a un elemento numerico
+    
+    char *token;
+    char *tokens[20]; 
+    int i = 0;
+    
+    token = strtok(version, "."); 
+    while (token != NULL && i < 10) {
+        tokens[i] = token; 
+        i++;
+        token = strtok(NULL, ".");
+    }
+    int cabezaVersion=atoi(tokens[0]);
+    
+    return cabezaVersion;
+}*/
+Version ExisteVersion(Archivo a, char version[]){
+    //Pre: Saber que la version que queremos trabajar no es la primera 
+    //Post: Si existe version te devuelve la version en la que queremos trabajar
+    //Si no existe te devuelve la Primera version 
+    
+    Archivo aux= a;
+    Version quiero= aux.versiones;
+    int i=0;
+    bool existe= false;
+    char actual[20];
+    while(version[i]!= NULL || existe == false){
+        
+        if(quiero->numero==version[i]-48){//si el numero es igual nos metemos en ese puntero || probando con codigo ASCII 
+            actual[i]=quiero->numero+48;
+            actual[i+1]='.';
+            i=i+2; // nos paramos en el siguiente valor del string de aux
+            if(strcmp(actual, version)==0){//si actual y version son iguales existe version y cortamo
+                existe= true;
+            }
+        }else if((quiero->hermano!=NULL)&&( quiero->hermano->numero==version[i]-48)){ //si la version en la que estamos parados tiene un hermano y esta version hermana nos sirve nos movemos
+            quiero=quiero->hermano;// solo me muevo en sub versiones (hacia la derecha) si el siguiente numero me sirve
+
+        }else if((quiero->hijo!=NULL)&& (quiero->hijo->numero>=version[i]-48)){//siempre que el hijo sea mayor o igual sigo bajando
+            quiero=quiero->hijo;
+        }
+        else{
+            i=i+2;//si no existe la version pero sigue habiendo que recorrer el string version lo recorremos 
         }
 
-
-    }
+    }      
+    if(existe)
+        return quiero;
+    else
+        return aux.versiones;
+        
 }
 TipoRet insertar_linea(Archivo &a,char * version,char * linea ,unsigned int nro_linea, char error){
 
